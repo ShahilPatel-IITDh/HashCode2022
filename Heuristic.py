c1a057c067244a7ebf5b170fb1e5dbb16424e2a5
Shahil Patel <shahilpatel809@gmail.com>
2022-02-25 02:27:29 +0530

    Updated Heuristic File

--- Heuristic.py
+++ Heuristic.py
@@ -6,19 +6,24 @@ class Contributer:
     def __init__(self, name, skills_level: dict):
         self.name = name
         self.skills_level = skills_level
+        self.available = 0
+    def make_contribute(self,time:int):
+        self.available = time
+    def update_round(self):
+        self.available -= 1
+
 
 class Project:
     def __init__(self, name, skills_level: dict, time:int, score:int, best_before:int, no_of_contributers:int):
         self.name = name
         self.skills_level = skills_level
         self.time = time
-        self.score = score
         self.best_before = best_before
         self.no_of_contributers = no_of_contributers
     
 
-no_of_contributers = int(inp_file.readline())
-no_of_projects = int(inp_file.readline())
+no_of_contributers, no_of_projects = map(int, inp_file.readline().split())
+
 contributers = []
 for i in range(no_of_contributers):
     contributer_line = inp_file.readline().split()
@@ -38,15 +43,29 @@ for i in range(no_of_projects):
     project_line = inp_file.readline().split()
     project_name = project_line[0]
     skills_level = {} #{skill,level}
-    project_skills = int(project_line[1])
+    project_skills = int(project_line[-1])
+    #print(project_line)
     for j in range(project_skills):
         project_skill = inp_file.readline().split()
         project_skill_name = project_skill[0]
         project_skill_level = int(project_skill[1])
         skills_level[project_skill_name] = project_skill_level
-    project = Project(project_name, skills_level, int(project_line[2]), int(project_line[3]), int(project_line[4]),int(project_line[5]))
+    project = Project(project_name, skills_level, int(project_line[1]), int(project_line[2]), int(project_line[3]),int(project_line[4]))
     #(name , skill , duration{time} , score , best_before , no. of contributors)
     projects.append(project)
 
+heuristic=[]
 for i in projects:
-    projects.project_line()
+    sum=0
+    #print(i.name)
+    for s in i.skills_level:
+        sum+=i.skills_level[s]
+    h=i.time*i.best_before*sum
+    heuristic.append([i,i.time*i.best_before*sum])
+
+def criteria(e):
+    return e[1]
+
+heuristic.sort(key=criteria)
+for i in heuristic:
+    print(i)
